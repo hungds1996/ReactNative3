da7b7772871617a8d7784a6b3a59c808b39e9c9b
HungDS <hungds1996@gmail.com>
2018-07-06 12:44:22 +0700

    session 1

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,58 +1,129 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react'
+import {View, Text, StyleSheet, Image} from 'react-native'
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class App extends Component {
+  state = {  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.title}>
+          <Text style={styles.textBig}>Schedule</Text>
+          <Image
+          style={{width: 100, height: 100, marginTop: 35, marginBottom:5}}
+          source={{uri: 'https://i.pinimg.com/originals/bd/6b/49/bd6b49482d53bbc4e770c8adec28344e.png'}}
+          />
+          <Text style={styles.textMedium}>FIFA WORLDCUP 2018</Text>
+        </View>
+        <View style={styles.table}>
+          <View style={styles.match}>
+            <Text style={styles.time}>Jul 06 2018 - 21:00</Text>
+            <View style={styles.teams}>
+              <Text style={styles.textNormal}>Uruguay</Text>
+              <Image 
+                source={{uri:'http://flags.fmcdn.net/data/flags/w580/uy.png'}}
+                style={styles.flag}
+              />
+              <Text style={styles.textNormal}>-</Text>
+              <Image 
+                source={{uri:'http://flags.fmcdn.net/data/flags/w580/fr.png'}}
+                style={styles.flag}
+              />             
+              <Text style={styles.textNormal}>France</Text>              
+            </View>
+          </View>
+
+          <View style={styles.match}>
+            <Text style={styles.time}>Jul 07 2018 - 01:00</Text>
+            <View style={styles.teams}>
+              <Text style={styles.textNormal}>Brazil</Text>
+              <Image 
+                source={{uri:'http://flags.fmcdn.net/data/flags/w580/br.png'}}
+                style={styles.flag}
+              />
+              <Text style={styles.textNormal}>-</Text>
+              <Image 
+                source={{uri:'http://flags.fmcdn.net/data/flags/w580/be.png'}}
+                style={styles.flag}
+              />             
+              <Text style={styles.textNormal}>Belgium</Text>              
+            </View>
+          </View>
+          <View style={styles.match}>
+            <Text style={styles.time}>Jul 08 2018 - 01:00</Text>
+            <View style={styles.teams}>
+              <Text style={styles.textNormal}>Russia</Text>
+              <Image 
+                source={{uri:'http://flags.fmcdn.net/data/flags/w580/ru.png'}}
+                style={styles.flag}
+              />
+              <Text style={styles.textNormal}>-</Text>
+              <Image 
+                source={{uri:'http://flags.fmcdn.net/data/flags/w580/hr.png'}}
+                style={styles.flag}
+              />             
+              <Text style={styles.textNormal}>Croatia</Text>              
+            </View>
+          </View>
+        </View>     
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
+  title: {
+    flex: 0.5,
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginBottom: 20
   },
-  welcome: {
-    fontSize: 20,
+  textBig: {
+    marginTop: '10%',
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  textMedium: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  textNormal:{
+    color: 'white'
+  },
+  container: {
+    backgroundColor: '#282F37',
+    flex: 1
+  },
+  table: {
+    flex: 0.5,
+  },
+  teams: {
+    paddingBottom: 5,
+    paddingLeft: 40,
+    paddingRight: 40,    
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  match: {
+    backgroundColor: '#3E465D',
+    borderRadius: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingTop: 5,
+    paddingBottom: 5,
+    marginBottom: 10
+  },
+  time: {
+    paddingBottom: 20,
     textAlign: 'center',
-    margin: 10,
+    color: 'white'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  flag: {
+    width: 20, 
+    height: 20, 
+    borderColor: 'white',
+    borderWidth: 1
+  }
 });
+
+export default App;
